cd15437c470ef811135faf36b0abe86af4f0b0a6
biamsarmento <biamsarmento@gmail.com>
2025-02-11 23:38:52 -0300

    fix github-pages

--- frontend/src/components/PopupWithForm.jsx
+++ frontend/src/components/PopupWithForm.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function PopupWithForm(props) {
-
-  const navigate = useNavigate();
 
   return (
     <section className={`${props.name} ${props.isOpen ? `${props.name}_opened` : ''}`}  id={props.name}>
